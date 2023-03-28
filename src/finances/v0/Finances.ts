28a620f78bf45d5e659c8b82092f471123f391df
ajmnz <arnau@ajb.cat>
2023-03-28 19:37:40 +0000

    update client to latest model changes

--- src/finances/v0/Finances.ts
+++ src/finances/v0/Finances.ts
@@ -12,7 +12,7 @@ export class Finances<SecurityDataType = unknown> {
   }
 
   /**
-   * @description Returns financial event groups for a given date range. **Usage Plan:** | Rate (requests per second) | Burst | | ---- | ---- | | 0.5 | 30 | For more information, see "Usage Plans and Rate Limits" in the Selling Partner API documentation.
+   * @description Returns financial event groups for a given date range. **Usage Plan:** | Rate (requests per second) | Burst | | ---- | ---- | | 0.5 | 30 | The `x-amzn-RateLimit-Limit` response header returns the usage plan rate limits that were applied to the requested operation, when available. The table above indicates the default rate and burst values for this operation. Selling partners whose business demands require higher throughput may see higher rate and burst values than those shown here. For more information, see [Usage Plans and Rate Limits in the Selling Partner API](https://developer-docs.amazon.com/sp-api/docs/usage-plans-and-rate-limits-in-the-sp-api).
    *
    * @name ListFinancialEventGroups
    * @request GET:/finances/v0/financialEventGroups
@@ -53,7 +53,7 @@ export class Finances<SecurityDataType = unknown> {
       }
     );
   /**
-   * @description Returns all financial events for the specified financial event group. **Usage Plan:** | Rate (requests per second) | Burst | | ---- | ---- | | 0.5 | 30 | For more information, see "Usage Plans and Rate Limits" in the Selling Partner API documentation.
+   * @description Returns all financial events for the specified financial event group. **Usage Plan:** | Rate (requests per second) | Burst | | ---- | ---- | | 0.5 | 30 | The `x-amzn-RateLimit-Limit` response header returns the usage plan rate limits that were applied to the requested operation, when available. The table above indicates the default rate and burst values for this operation. Selling partners whose business demands require higher throughput may see higher rate and burst values than those shown here. For more information, see [Usage Plans and Rate Limits in the Selling Partner API](https://developer-docs.amazon.com/sp-api/docs/usage-plans-and-rate-limits-in-the-sp-api).
    *
    * @name ListFinancialEventsByGroupId
    * @request GET:/finances/v0/financialEventGroups/{eventGroupId}/financialEvents
@@ -93,7 +93,7 @@ export class Finances<SecurityDataType = unknown> {
       ...params,
     });
   /**
-   * @description Returns all financial events for the specified order. **Usage Plan:** | Rate (requests per second) | Burst | | ---- | ---- | | 0.5 | 30 | For more information, see "Usage Plans and Rate Limits" in the Selling Partner API documentation.
+   * @description Returns all financial events for the specified order. **Usage Plan:** | Rate (requests per second) | Burst | | ---- | ---- | | 0.5 | 30 | The `x-amzn-RateLimit-Limit` response header returns the usage plan rate limits that were applied to the requested operation, when available. The table above indicates the default rate and burst values for this operation. Selling partners whose business demands require higher throughput may see higher rate and burst values than those shown here. For more information, see [Usage Plans and Rate Limits in the Selling Partner API](https://developer-docs.amazon.com/sp-api/docs/usage-plans-and-rate-limits-in-the-sp-api).
    *
    * @name ListFinancialEventsByOrderId
    * @request GET:/finances/v0/orders/{orderId}/financialEvents
@@ -123,7 +123,7 @@ export class Finances<SecurityDataType = unknown> {
       ...params,
     });
   /**
-   * @description Returns financial events for the specified data range. **Usage Plan:** | Rate (requests per second) | Burst | | ---- | ---- | | 0.5 | 30 | For more information, see "Usage Plans and Rate Limits" in the Selling Partner API documentation.
+   * @description Returns financial events for the specified data range. **Usage Plan:** | Rate (requests per second) | Burst | | ---- | ---- | | 0.5 | 30 | The `x-amzn-RateLimit-Limit` response header returns the usage plan rate limits that were applied to the requested operation, when available. The table above indicates the default rate and burst values for this operation. Selling partners whose business demands require higher throughput may see higher rate and burst values than those shown here. For more information, see [Usage Plans and Rate Limits in the Selling Partner API](https://developer-docs.amazon.com/sp-api/docs/usage-plans-and-rate-limits-in-the-sp-api).
    *
    * @name ListFinancialEvents
    * @request GET:/finances/v0/financialEvents
