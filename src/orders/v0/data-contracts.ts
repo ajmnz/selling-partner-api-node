7254831d777793c67c5e63bd11b9a62640419dd1
ajmnz <arnau@ajb.cat>
2023-07-26 19:12:15 +0000

    update client to latest model changes

--- src/orders/v0/data-contracts.ts
+++ src/orders/v0/data-contracts.ts
@@ -471,12 +471,10 @@ export interface PaymentExecutionDetailItem {
    * A sub-payment method for a COD order.
    *
    * Possible values:
-   *
-   * * COD - Cash On Delivery.
-   *
-   * * GC - Gift Card.
-   *
-   * * PointsAccount - Amazon Points.
+   * * `COD`: Cash On Delivery.
+   * * `GC`: Gift Card.
+   * * `PointsAccount`: Amazon Points.
+   * * `Invoice`: Invoice.
    */
   PaymentMethod: string;
 }
