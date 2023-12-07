e3226bd9218f1d8403bdd6a318fd8fdaddec6c80
ajmnz <arnau@ajb.cat>
2023-12-07 20:37:11 +0000

    update client to latest model changes

--- src/vendorDirectFulfillmentShipping/v20211228/data-contracts.ts
+++ src/vendorDirectFulfillmentShipping/v20211228/data-contracts.ts
@@ -114,9 +114,9 @@ export interface ShipmentDetails {
 export interface StatusUpdateDetails {
   /** This is required to be provided for every package and should match with the trackingNumber sent for the shipment confirmation. */
   trackingNumber: string;
-  /** Indicates the shipment status code of the package that provides transportation information for Amazon tracking systems and ultimately for the final customer. */
+  /** Indicates the shipment status code of the package that provides transportation information for Amazon tracking systems and ultimately for the final customer. For more information, refer to the [Additional Fields Explanation](https://developer-docs.amazon.com/sp-api/docs/vendor-direct-fulfillment-shipping-api-use-case-guide#additional-fields-explanation). */
   statusCode: string;
-  /** Provides a reason code for the status of the package that will provide additional information about the transportation status. */
+  /** Provides a reason code for the status of the package that will provide additional information about the transportation status. For more information, refer to the [Additional Fields Explanation](https://developer-docs.amazon.com/sp-api/docs/vendor-direct-fulfillment-shipping-api-use-case-guide#additional-fields-explanation). */
   reasonCode: string;
   /**
    * The date and time when the shipment status was updated. This field is expected to be in ISO-8601 date/time format, with UTC time zone or UTC offset. For example, 2020-07-16T23:00:00Z or 2020-07-16T23:00:00+01:00.
