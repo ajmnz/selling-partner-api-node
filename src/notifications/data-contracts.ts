b87f7fed3e02486a17f76ddaa9410897bb9f837a
ajmnz <arnau@ajb.cat>
2024-01-31 17:36:58 +0000

    update client to latest model changes

--- src/notifications/data-contracts.ts
+++ src/notifications/data-contracts.ts
@@ -23,7 +23,7 @@ export interface ProcessingDirective {
 export type EventFilter = AggregationFilter &
   MarketplaceFilter &
   OrderChangeTypeFilter & {
-    /** An `eventFilterType` value that is supported by the specific `notificationType`. This is used by the subscription service to determine the type of event filter. Refer to the section of the [Notifications Use Case Guide](https://developer-docs.amazon.com/sp-api/docs/notifications-api-v1-use-case-guide) that describes the specific `notificationType` to determine if an `eventFilterType` is supported. */
+    /** An `eventFilterType` value that is supported by the specific `notificationType`. This is used by the subscription service to determine the type of event filter. Refer to [Notification Type Values](https://developer-docs.amazon.com/sp-api/docs/notification-type-values) to determine if an `eventFilterType` is supported. */
     eventFilterType: "ANY_OFFER_CHANGED" | "ORDER_CHANGE";
   };
 
